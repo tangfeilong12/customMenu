625b1fae2bd740656d1a17a8f1002b3ae3f2a4cb
xmgcs <mac@tangfeilong.local>
2015-11-26 11:02:21 +0800

    添加图片

---
+++ customMenu/customMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customMenu
+//
+//  Created by mac on 15/11/26.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customMenu/customMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customMenu
+//
+//  Created by mac on 15/11/26.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
